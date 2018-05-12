849c4aa49bbf5690c5e63f7691d9ca2b1019d576
alyhamdy <36576749+alyhamdy@users.noreply.github.com>
2018-05-12 18:04:53 +0700

    work ali

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -11,8 +11,8 @@ class PostsController extends Controller
     // get last 6 posts in home page 
     public function getLastPosts(){
         
-        //$lastPosts = Post::orderBy('id', 'desc')->take(6)->get();
-        $lastPosts = Post::where('post_type_id', 2)->orderBy('id', 'desc')->take(6)->get();
+        $lastPosts = Post::orderBy('id', 'desc')->take(6)->get();
+        //$lastPosts = Post::where('post_type_id', 2)->orderBy('id', 'desc')->take(6)->get();
         
         return view('welcome', compact('lastPosts'));
     }
