36821083d8b5b56d38142f4d3f98450f9915f1c6
Mahmoud Gomaa <mahmoudg77@gmail.com>
2018-05-16 15:48:16 +0200

    Conflict resolve

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -11,14 +11,9 @@ class PostsController extends Controller
     // get last 6 posts in home page 
     public function getLastPosts(){
         
-<<<<<<< HEAD
+
         $lastPosts = Post::where('is_published',1)->where('post_type_id', 2)->orderBy('id', 'desc')->take(6)->get();
-=======
-        //$lastPosts = Post::orderBy('id', 'desc')->take(6)->get();
->>>>>>> d9bac107b88b2841ff5c00436d5aa83ea50e32d7
-        //$lastPosts = Post::where('post_type_id', 2)->orderBy('id', 'desc')->take(6)->get();
-        $lastPosts = Post::where('is_published',1)->where('post_type_id', 2)->orderBy('id', 'desc')->take(6)->get();
-        
+
         return view('welcome', compact('lastPosts'));
     }
     
