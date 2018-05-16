4839e59d1fe8b3a01be829f5c796a868690f1094
alyhamdy <36576749+alyhamdy@users.noreply.github.com>
2018-05-16 15:09:47 +0700

    update composer

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -11,13 +11,10 @@ class PostsController extends Controller
     // get last 6 posts in home page 
     public function getLastPosts(){
         
-<<<<<<< HEAD
         //$lastPosts = Post::orderBy('id', 'desc')->take(6)->get();
-        $lastPosts = Post::where('post_type_id', 2)->orderBy('id', 'desc')->take(6)->get();
-=======
+        //$lastPosts = Post::where('post_type_id', 2)->orderBy('id', 'desc')->take(6)->get();
         $lastPosts = Post::where('is_published',1)->orderBy('id', 'desc')->take(6)->get();
         //$lastPosts = Post::where('post_type_id', 2)->orderBy('id', 'desc')->take(6)->get();
->>>>>>> 7b19271eb686cd0208de1f75af1d78c579bbe69b
         
         return view('welcome', compact('lastPosts'));
     }
