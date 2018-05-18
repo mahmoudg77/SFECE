f2a861cf2fda0797bb64d8879e7f94efc07c948e
alyhamdy <36576749+alyhamdy@users.noreply.github.com>
2018-05-19 06:24:36 +0700

    edit by ali

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,16 +13,14 @@ class CategoryController extends Controller
         
         $allPostsByCat = post::where('category_id', '=', $id)->where('is_published',1)->get();
         $categoryName = category::where('id', $id)->first();
-<<<<<<< HEAD
         
         $lastPosts = Post::where('post_type_id', 2)->where('is_published', 1)->orderBy('id', 'desc')->take(4)->get();
         $allcats = Category::where('parent_id', '<>',null)->get();
-=======
 
         \App\Models\Visit::log(\App\Models\Category::class,$categoryName->id);
         $lastPosts = Post::where('post_type_id', 2)->orderBy('id', 'desc')->take(4)->get();
         $allcats = Category::all();
->>>>>>> 8bf4c445b8f281e2303d7ea8a1d9111a1f0d0518
+        
         return view('category', compact('allPostsByCat', 'categoryName', 'lastPosts', 'allcats'));
     }
     
